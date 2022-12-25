e183576908da8367843abaa71e435cba0acb6985
Jorge Obiols <jorge.obiols@gmail.com>
2022-12-25 12:50:17 -0300

    [FIX] corregir proyecto

--- test16_default/__manifest__.py
+++ test16_default/__manifest__.py
@@ -19,36 +19,32 @@
 ##############################################################################
 
 {
-    'name': 'test16',
-    'version': '16.0.1.0.0',
-    'category': 'Tools',
-    'summary': "Test for v16 CE",
-    'author': "jeo Software",
-    'website': 'http://github.com/jobiols/cl-test',
-    'license': 'AGPL-3',
-    'depends': [
-        ],
-    'installable': True,
-
+    "name": "test16",
+    "version": "16.0.1.0.0",
+    "category": "Tools",
+    "summary": "Test for v16 CE",
+    "author": "jeo Software",
+    "website": "http://github.com/jobiols/cl-test",
+    "license": "AGPL-3",
+    "depends": [],
+    "installable": True,
     # manifest version, if omitted it is backward compatible
-    'env-ver': '2',
-
+    "env-ver": "2",
     # if Enterprise it installs in a different directory than community
-    'odoo-license': 'CE',
-
+    "odoo-license": "CE",
     # Config to write in odoo.conf
-    'config': [],
-
-    'port': '8069',
-
-    'git-repos': [
-        'https://github.com/jobiols/cl-test.git',
+    "config": [
+        "workers = 0",
+        "admin_password = admin",
+    ],
+    "port": "8069",
+    "git-repos": [
+        "https://github.com/jobiols/cl-test.git",
         #'git@github.com:jobiols/odoo-jeo-ce.git',
     ],
-
     # list of images to use in the form 'name image-url'
-    'docker-images': [
-        'odoo jobiols/odoo-jeo:16.0',
-        'postgres postgres:10.1-alpine',
-    ]
+    "docker-images": [
+        "odoo jobiols/odoo-jeo:16.0",
+        "postgres postgres:15.1-alpine",
+    ],
 }
