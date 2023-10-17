9c766aad7b3eeb467a34aa00fcee0db3d7eae165
sistemassdeh <63413763+sistemassdeh@users.noreply.github.com>
2023-10-17 15:18:43 -0400

    Update __manifest__.py

--- arengy16_default/__manifest__.py
+++ arengy16_default/__manifest__.py
@@ -75,7 +75,7 @@
         'https://github.com/OCA/project-reporting -b 16.0',
         'https://github.com/OCA/purchase-workflow -b 16.0',
         'https://github.com/Yenthe666/auto_backup.git yenthe666/auto_backup -b 16.0',
-        'https://gitlab.com/guidoomuchiutti/ing_arengy-web.git',
+        'https://gitlab.com/guidoomuchiutti/ing_arengy-web.git -b 16.0',
 
     ],
     # list of images to use in the form 'name image-url'
